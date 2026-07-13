e4c945c439f6ca22e046f409bbce031b32b083d6
alperenatalay1 <alperenatalay720@gmail.com>
2026-07-13 18:42:42 +0300

    Update anasayfa.py

--- anasayfa.py
+++ anasayfa.py
@@ -3,12 +3,20 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #fafafa;
+        background-color: #fafafa !important;
+    }
+    h1, h2, h3 {
+        color: #a9043a !important;
+    }
+    div.stButton > button:first-child {
+        background-color: #fdb912 !important;
+        color: #a9043a !important;
+        border: 2px solid #a9043a !important;
+        font-weight: bold !important;
     }
     </style>
     """,
     unsafe_allow_html=True
-)
 st.sidebar.markdown("# 🏠 HOŞGELDİNİZ")
 import streamlit as st
 
